f2b0699e8f753d3ccca7ef01598493621fc27ca1
aperessini <steinaua@flip1.engr.oregonstate.edu>
2018-02-02 16:49:57 -0800

    Added unit tests

---
+++ projects/steinaua/dominion/unittest1.c
@@ -0,0 +1,72 @@
+/*----------------------
+ *Aaron Peressini
+ *CS 362 Winter 2018
+ *Assignment 3 : unittest1.c
+ *unit test for function numHandCards()
+ *
+ *Include the following in your makefile:
+ *
+ *unittest1: unittest1.c dominion.o rngs.o
+ *	gcc -o unittest1 unittest1.c -g dominion.o rngs.o $(CFLAGS)
+ *
+ *----------------------
+ */
+
+#include "dominion.h"
+#include <string.h>
+#include <stdio.h>
+#include "rngs.h"
+
+
+//Function being tested
+/*int numHandCards(struct gameState *state) {
+  return state->handCount[ whoseTurn(state) ];
+}*/
+
+int assertTrue(int statement)
+{
+	if(!statement)
+	{
+		//printf("Statement is false!\n");
+		return 0;
+	}
+
+	else
+	{	
+		//printf("Statement is true!\n");
+		return 1;
+	}
+}
+
+int main()
+{	
+	int i, j, handCount;
+	int maxHandCount = 5, seed = 500;
+	int k[10] = {adventurer, council_room, embargo, sea_hag, outpost, smithy, village, baron, tribute, great_hall};
+	struct gameState GS;
+
+	printf("TESTING FUNCTION numHandCards():\n");
+
+	for(i = 0; i < MAX_PLAYERS; i++)
+	{
+		for(handCount = -1; handCount < maxHandCount; handCount++)
+		{
+
+			printf("Player %d should have %d cards in their hand.\n", i, handCount);
+			memset(&GS, 23, sizeof(struct gameState)); //clear game state
+			j = initializeGame(MAX_PLAYERS, k, seed, &GS); //initialize new game
+			GS.whoseTurn = i;
+			GS.handCount[i] = handCount;
+			if(assertTrue(numHandCards(&GS) == handCount))
+				printf("Test passed.\n");
+			else
+				printf("Test failed.\n");
+
+		}
+
+	} 
+
+
+
+	return 0;
+}
---
+++ projects/steinaua/dominion/unittest4.c
@@ -0,0 +1,69 @@
+/*----------------------
+ *Aaron Peressini
+ *CS 362 Winter 2018
+ *Assignment 3 : unittest4.c
+ *unit test for function supplyCount()
+ *
+ *Include the following in your makefile:
+ *
+ *unittest4: unittest4.c dominion.o rngs.o
+ *	gcc -o unittest4 unittest4.c -g dominion.o rngs.o $(CFLAGS)
+ *
+ *----------------------
+ */
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include "rngs.h"
+
+
+
+int assertTrue(int statement)
+{
+	if(!statement)
+	{
+		//printf("Statement is false!\n");
+		return 0;
+	}
+
+	else
+	{	
+		//printf("Statement is true!\n");
+		return 1;
+	}
+}
+
+int main()
+{	
+	int i, j, c, supplycount;
+	int maxSupplyCount = 10, seed = 500;
+	int k[10] = {adventurer, council_room, embargo, sea_hag, outpost, smithy, village, baron, tribute, great_hall};
+	struct gameState GS;
+
+	printf("TESTING FUNCTION supplyCount():\n");
+
+	for(supplycount = 0; supplycount < maxSupplyCount; supplycount++)
+	{
+		for(c = curse; c < treasure_map; c++)
+		{
+
+			printf("Supply count of card %d should be %d.\n", c, supplycount);
+			memset(&GS, 23, sizeof(struct gameState)); //clear game state
+			j = initializeGame(MAX_PLAYERS, k, seed, &GS); //initialize new game
+			GS.whoseTurn = i;
+			GS.supplyCount[c] = supplycount;
+			if(assertTrue(supplyCount(c, &GS) == supplycount))
+				printf("Test passed.\n");
+			else
+				printf("Test failed.\n");
+
+		}
+
+	} 
+
+
+
+	return 0;
+}
